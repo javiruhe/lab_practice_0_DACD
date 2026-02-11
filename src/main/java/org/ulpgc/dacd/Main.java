eb0af9525448a1ce2709359d7bb6399fde080c96
javiruhe <javier.ruano102@alu.ulpgc.es>
2026-02-11 16:12:56 +0000

    added stating frame.

--- src/main/java/org/ulpgc/dacd/Main.java
+++ src/main/java/org/ulpgc/dacd/Main.java
@@ -1,11 +1,16 @@
 package org.ulpgc.dacd;
 
+import java.awt.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hola mundo")
+        System.out.println("Hola mundo");
+        Frame frame = new Frame();
+        frame.setSize(600, 400);
+        frame.setVisible(true);
     }
 }
